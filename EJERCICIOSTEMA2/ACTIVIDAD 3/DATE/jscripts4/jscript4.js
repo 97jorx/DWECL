0bc6aaa12fc57bdc4f9daecf41eaf27ce8d74848
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2019-10-09 14:54:09 +0200

    FIX EJERCICIO 4 DATE Y 16 STRING

--- EJERCICIOSTEMA2/ACTIVIDAD 3/DATE/jscripts4/jscript4.js
+++ EJERCICIOSTEMA2/ACTIVIDAD 3/DATE/jscripts4/jscript4.js
@@ -2,11 +2,11 @@ var date = new Date();
 var hora =  date.getHours();
 
 
-if (hora < 12)
+if (hora < 12) 
      document.write("Buenos días");
-else if (hora < 20)
+else if (hora >= 12 && hora <= 20)
      document.write("Buenos tardes");
-else
+else if (hora >= 20 && hora <= 23)
      document.write("Buenos noches");
 
 
--- EJERCICIOSTEMA2/ACTIVIDAD 3/STRING/jscripts16/jscript16.js
+++ EJERCICIOSTEMA2/ACTIVIDAD 3/STRING/jscripts16/jscript16.js
@@ -1,6 +1,6 @@
 var frase = prompt("Introduce una frase");
 var palabra = prompt("Introduce una palabra");
-
+var count = 0;
 
 var fraseArr = frase.split(' ');
 for (i = 0; i < fraseArr.length; i++) {
@@ -10,5 +10,7 @@ for (i = 0; i < fraseArr.length; i++) {
      }
 }
 
+
 frase = fraseArr.join(' ');
-document.write(frase);
+document.write(frase + " <br>");
+document.write("Longitud de la palabra" + palabra.length);

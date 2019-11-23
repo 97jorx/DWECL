f7dcd886f031b84b138bf473ca3a0d549b6dced2
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2019-11-23 18:57:05 +0100

    Ejercicio 2 valida por campos y da errores

--- EJERCICIOSJS/ACTIVIDADESTEMA5/COOKIESYVALIDACIONES/jscript2/jscript2.js
+++ EJERCICIOSJS/ACTIVIDADESTEMA5/COOKIESYVALIDACIONES/jscript2/jscript2.js
@@ -1,58 +1,29 @@
 
 
-var c = document.getElementById("canvas");
-var cuadrado = c.getContext("2d");
-var y = 5;
-var x = 5;
-cuadrado.fillRect(x, y, 50, 50);
-document.onkeydown = function (e) {
 
-    switch (e.key) {
-        case 'NumPad8':
-        case 'ArrowUp':
-            if (y > 10) {
-                cuadrado.clearRect(x, y, 500, 500);
-                v = y -= 20;
-                moverY(v);
-            }
-            break;
-        case 'NumPad2':    
-        case 'ArrowDown':
-            if (y < 439) {
-                cuadrado.clearRect(x, y, 500, 500);
-                v = y += 20;
-                moverY(v);
-            }
-            break;
-        case 'NumPad4':
-        case 'ArrowLeft':
-            if (x > 10) {
-                cuadrado.clearRect(x, y, 500, 500);
-                v = x -= 20;
-                moverX(v);
-            }
-            break;
-        case 'NumPad6':
-        case 'ArrowRight':
-            if (x < 439) {
-                cuadrado.clearRect(x, y, 500, 500);
-                v = x += 20;
-                moverX(v);
-            }
-            break;
-    }
-
-};
-
-
-function moverY(valor) {
-    cuadrado.beginPath();
-    cuadrado.fillRect(x, valor, 50, 50);
-    cuadrado.stroke();
+function validar(input) {
+     var elemento = document.getElementById(input.id);
+     var errores = document.getElementsByTagName('p');
+     console.log(elemento);
+     if (elemento.checkValidity()){
+          elemento.style.borderColor = 'green';
+          if(elemento.value == ""){
+               elemento.style.borderColor = 'red';
+          }
+     } else {
+          elemento.style.borderColor = 'red';
+          for (let i = 0; i < errores.length; i++) {
+               if (elemento.className == errores[i].className){
+                    errores[i].hidden = false;
+               } else if (elemento.className != errores[i].className) {
+                    errores[i].hidden = true;
+               }
+          }
+     }
+     
 }
 
-function moverX(valor) {
-    cuadrado.beginPath();
-    cuadrado.fillRect(valor, y, 50, 50);
-    cuadrado.stroke();
-}
+
+
+
+

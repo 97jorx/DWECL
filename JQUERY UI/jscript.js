8473081da8a677c53a7d1c638ce311fdab462fda
Jorge Cadena Casado <jorge.cadena@iesdonana.org>
2020-02-18 21:05:44 +0100

    JQUERY UI 1 completo

--- JQUERY UI/jscript.js
+++ JQUERY UI/jscript.js
@@ -1,10 +1,10 @@
 $(document).ready(() => {
-    letras = ['A','E','I','O','U'];
+    letras = ['A', 'E', 'I', 'O', 'U'];
     $("#sortable").sortable();
     $("#sortable").disableSelection();
     $("#perdiste").dialog({
         autoOpen: false,
-        modal: false,
+        modal: true,
         dialogClass: 'pdialog',
         title: 'HAS PERDIDO',
         show: "blind",
@@ -12,40 +12,50 @@ $(document).ready(() => {
     });
     $("#ganaste").dialog({
         autoOpen: false,
-        modal: false,
+        modal: true,
         dialogClass: 'gdialog',
         title: 'HAS GANADO',
         show: "blind",
         hide: "blind",
     });
-    
-    for(let i = letras.length - 1; i > 0; i--){
-        const j = Math.floor(Math.random() * 4)
-        const temp = letras[i]
-        letras[i] = letras[j]
-        letras[j] = temp
-    }
 
-    $('.draggable').each((i) => {
-        $(`#${i}`).text(letras[i])
-    });
-
+    shuffle();
     intentos = 2, i = 0;
     combinacion = "";
     $("#boton").click(() => {
         combinacion += $(".draggable").text();
         if (combinacion == "AEIOU") {
             $("#ganaste").dialog("open");
+            shuffle();
             intentos = 2;
         } else {
             intentos = intentos - 1;
+            $('#intentos').append(`<p>Intento ${intentos + 1}: ${combinacion}</p>`);
         }
         if (intentos === 0) {
             $("#perdiste").dialog("open");
+            $("span").click(() => {
+                $('#intentos').empty();
+            });
+            shuffle();
             intentos = 2;
         }
         combinacion = "";
+
     });
+
+
+    function shuffle() {
+        for (let i = letras.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * i)
+            const temp = letras[i]
+            letras[i] = letras[j]
+            letras[j] = temp
+        }
+        $('.draggable').each((i) => {
+            $(`#${i}`).text(letras[i])
+        });
+    }
 
 });
 
